be6a0d4035f31a39198bac76c7db4daa89aea0d4
Logeshwaran04 <65249556+Logeshwaran04@users.noreply.github.com>
2021-06-16 15:18:05 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const iconSearch = searchWrapper.querySelector(".icon");
+
+inputBox.onkeyup = (e) =>{
+    let userData = e.target.value;
+    let emptyArray =[];
+    if(userData)
+    {
+        emptyArray = suggestions.filter((data)=>{
+        
+           return   data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+             return data = '<li>'+ data + '</li>';
+        });
+        //console.log(emptyArray);
+        searchWrapper.classList.add('active');
+        showSuggestions(emptyArray);
+        let alList = suggBox.querySelectorAll('li');
+        for(let i =0 ; i< alList.length;i++)
+        {
+            alList[i].setAttribute("onclick","select(this)");
+        }
+    }
+    else{
+        searchWrapper.classList.remove('active');
+    }
+}
+
+function select(element)
+{
+    let selectUserData = element.textContent;
+    inputBox.value = selectUserData;
+    searchWrapper.classList.remove('active');
+
+}
+function showSuggestions(list)
+{
+    let listData;
+    if(!list.length)
+    {
+         userValue = inputBox.value;
+         listData = '<li>'+userValue+'</li>';
+    }
+    else
+    {
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
+iconSearch.onclick = () =>
+{
+    let inputValue = inputBox.value;
+    let linkWord = "https://www.google.com/search?q=".concat( inputValue.split(" ").join('+'));
+    //console.log(linkWord);
+    location.replace(linkWord);
+}
+
